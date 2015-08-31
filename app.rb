0ad581c6ad2cfcca8c59e65076cdc03ebf7c765c
Lucy Davies <lucy@lucymhdavies.com>
2015-09-01 00:38:48 +0100

    Stupidly basic mongoid first steps

--- app.rb
+++ app.rb
@@ -1,12 +1,29 @@
 require 'sinatra'
 require 'dotenv'
+require 'mongoid'
 
 Dotenv.load
 
-
+Mongoid.load!("mongoid.yml", :development)
 
 get '/' do
-	"Hello Sinatra"
+	bob = Person.create( first_name: "Bob" )
+
+
+	"Hello. #{bob.first_name}"
+end
+
+class Person
+	include Mongoid::Document
+
+	field :first_name,  type: String
+	field :middle_name, type: String
+	field :last_name,   type: String
+
+	def first_name
+		"My name is #{self[:first_name]}"
+	end
+
 end
 
 
